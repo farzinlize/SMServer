af0fb626297f26516767bf1617160f581d7a4970
farzin <farzinlize@live.com>
2020-02-10 11:55:37 +0330

    add partition (int and string bundle)

---
+++ host/utils/Partition.java
@@ -0,0 +1,13 @@
+package host.utils;
+
+public class Partition{
+    
+    public String data;
+    public int index;
+
+    public Partition(int index, String data){
+        this.data = data;
+        this.index = index;
+    }
+
+}
